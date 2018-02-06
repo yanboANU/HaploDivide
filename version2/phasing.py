3413fc7db357dd973e1ebf89d0b5de11970e28a6
Yanbo Li <admin-u6260133@cecs-044449.cecs.anu.edu.au>
2018-02-06 16:55:41 +1100

    undebug version

--- version2/phasing.py
+++ version2/phasing.py
@@ -11,9 +11,33 @@ import tools
 import column
 
 class Phasing:
+ 
+    def pre_init(self):
 
-    def __init__(self, columns,  mutation, delete, insert, stable, obLen = 3):
-        self_colums = columns
+        mutation = []
+        insert = []
+        delete = []
+        stable = []  
+        for (refPos, c) in _columns.items():
+            c._set_Lable()
+            #sys.exit()
+            if c._is_insert == 1:
+                insert.append(refPos)
+        
+            if c._is_mutation == 1:
+                mutation.append(refPos)
+        
+            if c._is_delete == 1:
+                delete.append(refPos)
+        
+            if c._is_stable == 1:
+                stable.append(refPos)
+        return stable, mutation, delete, insert
+
+    def __init__(self, columns, obLen = 3):
+        self._colums = columns
+
+        stable, mutation, delete, insert = pre_init() 
         
         self._stableRange = tools.pos_2_Range(stable)    
         
@@ -23,8 +47,16 @@ class Phasing:
         self._snp_delete = delete(delete, obLen)
         self._snp_insert = insert(insert, obLen)
 
+
+        # output
         # temporary 
-        self._snp = self._snp_mutation 
+        self._snp = self._snp_mutation
+        self._coverage = []
+        self._label0 = ""
+        self._label1 = ""
+        self.phase1 = []
+        self.phase2 = [] 
+           
 
     def _get_SNP(self, pos, obLen):
         # unfinish 
@@ -40,55 +72,106 @@ class Phasing:
         readsLabel = {} 
 	for i in range(len(_snp)):
 	    p = _snp[i] 
-	    (nuc, supportReadName) = _columns[p]._map_content
-		supports = [] #  
-		sortedNuc = sorted_Map_Value(nuc)
-		a = sortedNuc[0][0]
-		b = sortedNuc[1][0]
-		for (k, v) in supportReadName.items():
-		    #print (k,v)
-		    if k != a and k != b:
-			supports.extend(v)
-		#print (supports)
-		#print ("bbb")
-		#sys.exit("stop")
-		for readId in supportReadName[a]:
-		    if readId not in readsFlag:
-			readsFlag[readId] = [3]*len(ObSNP)
-		    readsFlag[readId][i] = 0
+	    content = _columns[p]._map_content
+	    #a = content[0][0]
+	    #b = content[1][0]
+	    for readId in content[0][1]:
+	        if readId not in readsLabel:
+		    readsLabel[readId] = [3]*len(_snp)
+		readsLabel[readId][i] = 0
 	 
-		for readId in supportReadName[b]:
-		    if readId not in readsFlag:
-			readsFlag[readId] = [3]*len(ObSNP)
-		    readsFlag[readId][i] = 1
+	    for readId in content[1][1]:
+		if readId not in readsLabel:
+	            readsLabel[readId] = [3]*len(_snp)
+		readsLabel[readId][i] = 1
+            j = 2  
+            while j < len(content): 
+         	for readId in content[j][1]:
+		    if readId not in readsLabel:
+		        readsLabel[readId] = [3]*len(_snp)
+		    readsLabel[readId][i] = 2
+	'''
+        sortFlag = sorted_Map_Value(readsLabel, False)
+	print ("show")
+	for ele in sortFlag:
+        print (ele)
+        ''' 
+    return readsLabel       
 
-		for readId in supports:
-		    if readId not in readsFlag:
-			readsFlag[readId] = [3]*len(ObSNP)
-		    readsFlag[readId][i] = 2
-	    sortFlag = sorted_Map_Value(readsFlag, False)
-	    print ("show")
-	    for ele in sortFlag:
-		print (ele)
- 
-    return readsFlag       
+    def _phasing(window=3):
+            
+        readsLabel = _label_reads()
+        _coverage = (len(_snp) - window)*[0]   
+        for i in range(len(_snp)-window):
+            phases = {}
+            for (read, label) in readsLabel.items():
 
-def phasing(usefulPhases, phase0, phase1, label0, label1, readsSupport):
+                f = ''.join( str(j) for j in label[ i :i+window] )
+                    if f not in phases:
+                        phases[f] = []
+                phases[f].append(read)
+        #print (phases)   
+        
+            print (_snp[i:i+3])
+            sortedPhases = tools.sorted_Map_Value_Len(phases)
+            cov = 0
+            allCoverPhases = []
+            for (label, reads) in sortedPhases:
+                if label.find('3') == -1:
+                    cov += len(reads)
+                    allCoverPhases.append((label, len(reads)))
+            _coverage.append(cov)  
+
+            if len(allCoverPhases) == 1:
+    
+
+            elif ( len(allCoverPhases)>=2 and allCoverPhases[0][1] > cov * 0.2 and allCoverPhases[1][1] > cov * 0.2
+               and tools.is_Bool_Reverse(allCoverPhases[0][0], allCoverPhases[1][0]) ):
+                _phasing_one_window(allCoverPhases, phases)
+                #sys.exit()
+            else:
+                print ("not statified")
+                #reclass_Intersection(phase0, phase1, label0, label1, readsFlag)
+                #fout.write("%s %s\n" % (label0, phase0))
+                #fout.write("%s %s\n" % (label1, phase1))
+                #fout.write("\n")
+                #sys.exit()
+
+
+           
+    def _phasing_one_window(allCoverPhases, labelReads):
      
-    print ("in phasing")
-    print (label0 , label1)
-    print (usefulPhases[0][0], usefulPhases[1][0])
-    if len(label0) == 0: 
-        label0 = usefulPhases[0][0]
-        label1 = usefulPhases[1][0] 
-        #phase0.extend(readsSupport[label0])
-        #phase1.extend(readsSupport[label1])
-    else:
-        assert ( label0[-2:] == usefulPhases[0][0][:2] or  
-                 label0[-2:] == usefulPhases[1][0][:2] )
-        if label0[-2:] == usefulPhases[0][0][:2]: 
-            label0 = label0 + usefulPhases[0][0][-1] 
+        print ("in phasing")
+        print (label0 , label1)
+        print (allCoverPhases[0][0], allCoverPhases[1][0])
+        if len(label0) == 0: 
+            _label0 = allCoverPhases[0][0]
+            _label1 = allCoverPhases[1][0] 
+        else:
+            assert ( _label0[-2:] == allCoverPhases[0][0][:2] or  
+                   _label0[-2:] == allCoverPhases[1][0][:2] )
+            if _label0[-2:] == allCoverPhases[0][0][:2]: 
+                _label0 = _label0 + allCoverPhases[0][0][-1] 
+                _label1 = _label1 + allCoverPhases[1][0][-1] 
+            elif _label0[-2:] == allCoverPhases[1][0][:2]:     
+                _label0 = _label0 + allCoverPhases[1][0][-1]
+                _label1 = _label1 + allCoverPhases[0][0][-1]
+        else:
+            print ("error type1")
 
+   
+        for v in allCoverPhases:
+            if tools.hamming_Distance(v[0], _label0[-3:]) < tools.hamming_Distance(v[0], _label1[-3:]):
+                _phase0.extend(labelReads[v[0]])
+ 
+            elif tools.hamming_Distance(v[0], _label0[-3:]) > tools.hamming_Distance(v[0], _label1[-3:]):
+                _phase1.extend(labelReads[v[0]])
+            else:
+                print ("same distance", v)
+            
+
+        print (_label0, len(_phase0), _phase0)      
+        print (_label1, len(_phase1), _phase1)      
         
    
-
+        print ("intersection:", set(_phase0).intersection(set(_phase1)) )
