df4dbff8d14886b6ffec3bfec11f83aca1302c5a
Yanbo Li <admin-u6260133@cecs-044449.cecs.anu.edu.au>
2018-03-13 14:18:25 +1100

    add stat.py and read.py

--- version2/read.py
+++ version2/read.py
@@ -1,4 +1,31 @@
 import sys
+import column
+
+
+
+def read_align(filename):
+    # read align_pos 
+    f = open(filename, "r")
+    ref_pos = []
+    contig_pos = []
+    ref_label = 1 
+    for line in f:
+        words = line.split(',')
+        if words[0].isdigit():
+            if len(ref_pos) == 0:
+                for c in words:
+                    ref_pos.append(int(c))   
+            else: 
+                for c in words:
+                    contig_pos.append(int(c))
+    
+    assert len(ref_pos) == len(contig_pos)
+    '''
+    fout = open("ref_contig", "w")  
+    for i in range(len(ref_pos)):
+        fout.write("%s %s\n" % (ref_pos[i], contig_pos[i]))
+    '''
+    return ref_pos, contig_pos
 
 def read_mutation_list(filename):
     f = open(filename, "r")
@@ -16,10 +43,21 @@ def read_columns(filename):
     f = open(filename, "r")
     columns = {}
     for line in f:
+        words = line.strip().split() 
         if line.startswith("reference"):
-            words = line.split() 
-            pos = int(words[1]) 
-            assert not in columns
-            columns[ words[1] ] = []
-        else: 
-             
+            #print (words[1])
+            pos = int(words[2]) 
+            assert pos not in columns
+            columns[pos] = column.Column(pos)
+            columns[pos]._cov = int(words[4])  
+        elif (len(words) > 0 ):
+            #columns[pos]._print()
+            columns[pos]._diff_cov.append(int(words[1]))
+    for pos in columns:
+        columns[pos]._diff_cov.sort() 
+        columns[pos]._diff_cov.reverse() 
+    #columns[0]._print()
+    #columns[2925]._print() 
+    return columns       
+#read_columns(sys.argv[1]) 
+#read_align(sys.argv[1])
--- version2/stat.py
+++ version2/stat.py
@@ -1,10 +1,89 @@
 import sys
-import column
-
-def stat_column(filename):
-    columns = read.read_columns(filenae)
+import read
 
 
+def stat_pre_snp_cov(preMfile, columnsfile):
+
+    pre_mutation = read.read_mutation_list(preMfile)
+    #record in reference pos
+    columns = read.read_columns(columnsfile)
+
+
+    fout = open("pre_snp_first_cov", "w")    
+ 
+    fout2 = open("pre_snp_second_cov", "w")     
+    for pos in pre_mutation:
+        fout.write("%s\n" % (columns[pos]._diff_cov[0]))
+        #fout.write("%s\n" % (columns[pos]._diff_cov[0]/columns[pos]._cov))
+        columns[pos]._print()
+        if pos not in columns:
+            break    
+        if len(columns[pos]._diff_cov) == 1:
+            fout2.write("0\n")
+        else:
+            fout2.write("%s\n" % (columns[pos]._diff_cov[1]))
+            #fout2.write("%s\n" % (columns[pos]._diff_cov[1]/columns[pos]._cov))
+
+    fout.close()
+    fout2.close() 
+
+def stat_real_snp_cov(realMfile, columnsfile, alignfile):
+
+    real_mutation = read.read_mutation_list(realMfile)
+    #record in reference pos
+    columns = read.read_columns(columnsfile)
+
+    ref_pos, contig_pos = read.read_align(alignfile) # blasr result
+    real_mutation_in_contig = []
+    #print ("aaa")
+    align_map = {}
+    for i in range(len(ref_pos)):
+        if isinstance(ref_pos[i], int) and isinstance(contig_pos[i], int): 
+            align_map[ref_pos[i]]= contig_pos[i]
+
+
+    for pos in real_mutation:
+        # some position of reference donot have alignment
+        if pos not in align_map:
+            continue     
+        real_mutation_in_contig.append(align_map[pos])
+        #print (pos, align_map[pos])
+    #cover reference pos to contig pos
+    fout = open("snp_first_cov_ratio", "w")    
+ 
+    fout2 = open("snp_second_cov_ratio", "w")     
+    for pos in real_mutation_in_contig:
+        #fout.write("%s\n" % (columns[pos]._diff_cov[0]))
+        fout.write("%s\n" % (columns[pos]._diff_cov[0]/columns[pos]._cov))
+        columns[pos]._print()
+        if pos not in columns:
+            break    
+        if len(columns[pos]._diff_cov) == 1:
+            #fout2.write("0\n")
+            fout2.write("0\n")
+
+        else:
+            #fout2.write("%s\n" % (columns[pos]._diff_cov[1]))
+            fout2.write("%s\n" % (columns[pos]._diff_cov[1]/columns[pos]._cov))
+    fout.close()
+    fout2.close() 
+
+
+def stat_distance_snp(snpMfile,snpDisfile):
+
+    snp_pos = read.read_mutation_list(snpMfile)
+    #record in reference pos
+
+    fout = open(snpDisfile, "w")    
+    
+    i=1 
+    while i < len(snp_pos):
+        fout.write("%s\n" % (snp_pos[i] - snp_pos[i-1]))
+        i += 1
+    fout.close()
 
 if __name__ == '__main__':
 
+    #stat_real_snp_cov(sys.argv[1], sys.argv[2], sys.argv[3])
+    #stat_pre_snp_cov(sys.argv[1], sys.argv[2])
+    stat_distance_snp(sys.argv[1], sys.argv[2])
