472c23633f840a968dded928b359cf2ee2060499
Yu Lin <yulin@bombus.anu.edu.au>
2018-06-27 09:40:31 +1000

    1st and 2nd frequence

---
+++ HaploCheck/get_1st_2st_frequence.py
@@ -0,0 +1,78 @@
+import sys
+import read
+import tools  
+
+def read_columns(filename):
+    columns= {}
+    f = open(filename, "r")
+    for line in f:
+        #print line
+        words = line.strip().split(" ")
+        #print len(words)
+        if len(words) <= 1:
+            continue
+        if words[0].startswith("reference"):
+            pos = int(words[2])
+            coverage = int(words[5]) 
+        else:
+            if pos not in columns:
+                columns[pos] = []
+            columns[pos].append(float(words[1])/coverage)
+            #print pos, columns[pos]
+    for key in columns:
+        columns[key] = sorted(columns[key], reverse=True)
+    return columns    
+    
+
+    
+
+if __name__ == "__main__":
+    
+    
+    real_snpPosition, real_snpContent = read.read_snp(sys.argv[1]) # mutation_record
+    columns = read_columns(sys.argv[2]) # 
+   
+    SNPCount = {}
+    for key in real_snpPosition:
+        if key not in columns:
+            continue
+        if len(columns[key]) <= 1:
+            #print columns[key][0], 0
+            if (columns[key][0], 0) not in SNPCount:
+                SNPCount[ (columns[key][0], 0)] = 0
+            SNPCount[ (columns[key][0], 0)] += 1
+        else:
+            #print columns[key][0], columns[key][1]
+            if (columns[key][0], columns[key][1]) not in SNPCount:
+                SNPCount[ (columns[key][0], columns[key][1]) ] = 0
+            SNPCount[ (columns[key][0], columns[key][1]) ] += 1
+
+
+    nonSNPCount = {}
+    for key in columns:
+        if key not in real_snpPosition:
+            if len(columns[key]) <= 1:
+                if (columns[key][0], 0) not in nonSNPCount:
+                    nonSNPCount[ (columns[key][0], 0) ] = 0
+                nonSNPCount[ (columns[key][0], 0) ] += 1
+            else:
+                if (columns[key][0], columns[key][1]) not in nonSNPCount:
+                    nonSNPCount[ (columns[key][0], columns[key][1]) ] = 0
+                nonSNPCount[ (columns[key][0], columns[key][1]) ] += 1
+
+    
+    print "real SNP Position"
+    for (v1,v2) in sorted(SNPCount.items()):
+        print ('%.2f %.2f %d ' % (v1[0], v1[1], v2))
+
+
+    print "non SNP Position"
+    '''
+    for key in nonSNPCount:
+        print key, nonSNPCount[key]     
+    '''
+
+    for (v1,v2) in sorted(nonSNPCount.items()):
+        #print v1[0], v1[1], v2
+        print ('%.2f %.2f %d ' % (v1[0], v1[1], v2))
+
